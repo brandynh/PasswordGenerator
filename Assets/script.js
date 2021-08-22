8a09aa8b0405aba3140fa2c5d32049853aa31c98
Brandyn <hufferbrandyn@gmail.com>
2021-08-21 20:09:47 -0500

    Initiate the beginning of the password generator

---
+++ Assets/script.js
@@ -0,0 +1,38 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var letters = [];
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+// FUNCTIONS
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Constant Object with -
+
+const randomFunction = {
+  lower: 'abcdefghijklmnopqrstuvwxyz',
+  upper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  number: '1234567890',
+  symbol: '!@#$%^&*(){}[]=<>/,.'
+}
+
+// Generate password function
+function generatePassword() {
+
+}
+
