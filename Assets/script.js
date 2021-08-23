488d7ec7a63444bbc67a0ebedaf2646b47a3c64b
Brandyn <hufferbrandyn@gmail.com>
2021-08-22 19:00:13 -0500

    implement do while look for intial password length prompt

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var letters = [];
+var includedChars = [];
+
+const charSets = {
+  lower: "abcdefghijklmnopqrstuvwxyz",
+  upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  number: "1234567890",
+  symbol: "!@#$%^&*(){}[]=<>/,."
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
 
 
 
@@ -22,18 +28,30 @@ function writePassword() {
 
 }
 
+
+
+
 // Constant Object with strings of potential character sets
 
-const charSets = {
-  lower: 'abcdefghijklmnopqrstuvwxyz',
-  upper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  number: '1234567890',
-  symbol: '!@#$%^&*(){}[]=<>/,.'
-}
+
 
 // Generate password function
 function generatePassword() {
   
+  // Do while runs until valid information is entered 
+ 
+ do {
+    var userPassLength = window.prompt("How many characters would you like to include in your password? Must be between 8 and 128 characters.");
+    
+    if (userPassLength < 8 || userPassLength > 128 || isNaN(userPassLength)) {
+
+      alert("Please enter a valid value.");
+    }
+
+  } while (userPassLength < 8 || userPassLength > 128 || isNaN(userPassLength));
+
+
+
 
 }
 
